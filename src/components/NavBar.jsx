1c69b8ae4f320fc8b40fd8e07a3955d54e9aaa5d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-05-29 16:37:30 -0700

    updates: 5-29-24

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import headerGraphic from '../../public/header-graphic.png';
 
 const NavBar = ({ openModal }) => {
   return (
     <nav className="w-full p-4 flex justify-between items-center text-white fixed top-0 bg-black bg-opacity-70 z-10">
       <div className="flex items-center">
-        <img
-          src={headerGraphic}
-          alt="LuxGirl OG - Original Geek"
-          className="h-32 cursor-pointer"
+        <h1
+          className="text-3xl font-bold font-creative cursor-pointer"
           onClick={openModal}
-        />
+        >
+          LuxGirl OG - Original Geek !
+        </h1>
       </div>
       <div>
         <Link to="/" className="mx-2 p-2 hover:bg-white hover:text-black transition-colors">Gallery</Link>
